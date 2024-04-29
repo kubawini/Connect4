c92a1984a464ec04f6f19131f95ceb5c9a6d7aa5
Filip <fillipser143@gmail.com>
2024-04-29 12:15:29 +0200

    Remove unused targets

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,21 +6,8 @@ plugins {
 
 korge {
 	id = "com.sample.demo"
-
-// To enable all targets at once
-
-	//targetAll()
-
-// To enable targets based on properties/environment variables
-	//targetDefault()
-
-// To selectively enable targets
-	
 	targetJvm()
 	targetJs()
-    targetWasm()
-	targetDesktop()
-	targetIos()
 	targetAndroid()
 
 	serializationJson()
